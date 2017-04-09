e3fb417234c658a22edc61160ca0290d18753333
brandonxiang <1542453460@qq.com>
2017-04-09 23:50:43 +0800

    divide router into another folder

--- src/main.js
+++ src/main.js
@@ -3,37 +3,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import FastClick from 'fastclick';
-import VueRouter from 'vue-router';
 import vuexI18n from 'vuex-i18n';
 import VueLeaflet from 'vueleaflet';
 
+import router from './router/index'
 import App from './App';
-import Setting from './components/Setting';
-import MapCtrl from './components/Map';
-import Discovery from './components/Discovery';
-import About from './components/About';
 import module from './vuex/store';
 import locales from './locales/locales'
 
 FastClick.attach(document.body);
-
-Vue.use(VueRouter);
-
-const routes = [{
-  path: '/',
-  component: MapCtrl,
-}, {
-  path: '/setting',
-  component: Setting,
-}, {
-  path: '/discovery',
-  component: Discovery,
-}, {
-  path: '/about',
-  component: About,
-}];
-
-const router = new VueRouter({ routes });
 
 Vue.use(Vuex);
 
@@ -53,24 +31,24 @@ Vue.i18n.set('zh-CN');
 
 const history = window.sessionStorage
 history.clear()
-let historyCount = history.getItem('count') * 1 || 0
-history.setItem('/', 0)
+let historyCount = history.getItem('count')*1 || 0
+history.setItem('/',0)
 
-router.beforeEach(function (to, from, next) {
+router.beforeEach(function(to, from, next){
   const toIndex = history.getItem(to.path)
   const fromIndex = history.getItem(from.path)
 
-  if (toIndex) {
+  if(toIndex){
     if (toIndex > fromIndex || !fromIndex || (toIndex === '0' && fromIndex === '0')) {
-      store.commit('updateDirection', { direction: 'forward' })
+      store.commit('updateDirection', {direction: 'forward'})
     } else {
-      store.commit('updateDirection', { direction: 'reverse' })
+      store.commit('updateDirection', {direction: 'reverse'})
     }
   } else {
     ++historyCount
     history.setItem('count', historyCount)
     to.path !== '/' && history.setItem(to.path, historyCount)
-    store.commit('updateDirection', { direction: 'forward' })
+    store.commit('updateDirection', {direction: 'forward'})
   }
 
   if (/\/http/.test(to.path)) {
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+
+import Setting from '../components/Setting';
+import MapCtrl from '../components/Map';
+import Discovery from '../components/Discovery';
+import About from '../components/About';
+
+
+Vue.use(VueRouter);
+
+const routes = [{
+  path: '/',
+  component: MapCtrl,
+}, {
+  path: '/setting',
+  component: Setting,
+}, {
+  path: '/discovery',
+  component: Discovery,
+}, {
+  path: '/about',
+  component: About,
+}];
+
+const router = new VueRouter({ routes })
+
+export default router
