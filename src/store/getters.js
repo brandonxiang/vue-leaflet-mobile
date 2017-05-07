8488649a41ad149e798c6418c61c1a5e7616bb12
brandonxiang <1542453460@qq.com>
2017-05-07 14:44:50 +0800

    hot fix

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,3 +3,7 @@ export const selectedCities =  (state) => state.cities.filter(city => state.sele
 export const title = state => state.title
 
 export const direction = state => state.direction
+
+export const center = state => state.center
+
+export const cityName = state => state.cityName
